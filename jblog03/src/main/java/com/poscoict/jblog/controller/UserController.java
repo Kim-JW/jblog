66e97ddb3e770851490f2bb00a0dc785ed5b0fe1
Kim-JW <bbam03@naver.com>
2022-02-04 17:12:45 +0900

    first commit (Vo & base structure & join test)

---
+++ jblog03/src/main/java/com/poscoict/jblog/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.poscoict.jblog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.poscoict.jblog.service.UserService;
+import com.poscoict.jblog.vo.UserVo;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
+	public String join() {
+		return "user/join";
+	}
+	
+	@RequestMapping(value = "/join", method = RequestMethod.POST)
+	public String join(UserVo vo) {
+		userService.join(vo);
+		return "redirect:/user/joinsuccess";
+	}
+	
+	@RequestMapping(value = "/joinsuccess")
+	public String joinsuccess() {
+		return "user/joinsuccess";
+	}
+}
